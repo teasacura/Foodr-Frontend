63d0f1fdd9c394c5b8ff830dfb899266378797ce
teasacura <teasacuraster@gmail.com>
2018-07-24 12:30:34 -0400

    main restaurant view is working

--- src/components/RestaurantShow.js
+++ src/components/RestaurantShow.js
@@ -2,10 +2,26 @@ import React from "react";
 
 
 const RestaurantShow = (props) => {
-    const { name } = props.selectedRestaurant
+    const { image_url, name, address, url, is_closed, reviews, rating, do_delivery } = props.selectedRestaurant
     return (
       <div>
+        <div className="ui small centered image">
+          <img src={image_url} styles={{maxHeight: '10px'}} alt=""></img>
+        </div>
         <h1>{name}</h1>
+        <h3>{address}</h3>
+        <div className="ui four column doubling stackable grid container">
+          <div className="column">
+            <p>Number of Reviews: {reviews}</p>
+            <p>Rating: {rating} </p>
+            <p>{do_delivery ? "Delivers Food" : "No Food Delivery"}</p>
+          </div>
+          <div className="column">
+            <p><a href={url}>Website</a></p>
+            <p>{is_closed ? "Currently Closed" : "Currently Open" }</p>
+          </div>
+        </div>
+
       </div>
     );
 }
