1bbde2e8e9aed5544a6767c7ef55c26f8a8a631d
teasacura <teasacuraster@gmail.com>
2018-07-24 13:36:44 -0400

    with Google maps. learning how to manage the map

--- src/components/MapWithMarkedInfoWindows.js
+++ src/components/MapWithMarkedInfoWindows.js
@@ -1,3 +1,4 @@
+import React from 'react';
 const { compose, withProps, withStateHandlers } = require("recompose");
 const FaAnchor = require("react-icons/lib/fa/anchor");
 const {
@@ -34,9 +35,11 @@ const MapWithAMarkedInfoWindow = compose(
   </GoogleMap>
 );
 
-<MapWithAMarkedInfoWindow
-  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+export default MapWithAMarkedInfoWindow;
+
+{/* <MapWithAMarkedInfoWindow
+  googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&v=3.exp&libraries=geometry,drawing,places`}
   loadingElement={<div style={{ height: `100%` }} />}
   containerElement={<div style={{ height: `400px` }} />}
   mapElement={<div style={{ height: `100%` }} />}
-/>
+/> */}
--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,9 +1,55 @@
 import React from "react";
+import { apiKey } from '../key.js'
 import RestaurantList from "./RestaurantList";
 import RestaurantShow from "../components/RestaurantShow";
+// import MapWithAMarkedInfoWindow from "../components/MapWithAMarkedInfoWindow";
 // import EditHobbit from "./EditHobbit";
 import { fetchInitialRestaurants } from "../actions";
 import { connect } from "react-redux";
+const { compose, withProps, withStateHandlers } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+} = require("react-google-maps");
+
+
+// function getLongituteLatitude(position) {
+//   return position
+// }
+//
+// const position = navigator.geolocation.getCurrentPosition(getLongituteLatitude)
+// const { latitude, longitude } = position.coords
+
+const MapWithAMarkedInfoWindow = compose(
+  withStateHandlers(() => ({
+    isOpen: false,
+  }), {
+    onToggleOpen: ({ isOpen }) => () => ({
+      isOpen: !isOpen,
+    })
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultZoom={8}
+    // defaultCenter={{ lat: latitude, lng: longitude }}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    <Marker
+      // position={{ lat: latitude, lng: longitude }}
+      position={{ lat: -34.397, lng: 150.644 }}
+      onClick={props.onToggleOpen}
+    >
+      {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+        <p>HELLO WORLD</p>
+      </InfoWindow>}
+    </Marker>
+  </GoogleMap>
+);
 
 class RestaurantsContainer extends React.Component {
   componentDidMount() {
@@ -22,7 +68,12 @@ class RestaurantsContainer extends React.Component {
           {this.props.restaurants.selectedRestaurant !== null ? (
             <RestaurantShow selectedRestaurant={this.props.restaurants.selectedRestaurant} />
           ) : (
-            <h3>select a restaurant</h3>
+            <MapWithAMarkedInfoWindow
+              googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&v=3.exp&libraries=geometry,drawing,places`}
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `400px` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+            />
           )}
         </div>
       </div>
