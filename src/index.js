5e513afee02b35ee0da5be3e56bad982f73b3d55
teasacura <36260380+teasacura@users.noreply.github.com>
2018-10-24 17:41:59 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
@@ -10,9 +9,7 @@ import rootReducer from "./reducers";
 import thunk from "redux-thunk"
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 ReactDOM.render(<Provider store={store}>
   <App />
 </Provider>, document.getElementById('root'));
-registerServiceWorker();
