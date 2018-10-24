1e2e6bd3f02532038f5fc55f119ea8078ecb4ba7
teasacura <36260380+teasacura@users.noreply.github.com>
2018-10-24 17:38:45 -0400

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { logIn, createNewUser } from '../actions'
 import { connect } from "react-redux";
-// import { Link, Redirect } from 'react-router-dom'
 
 class SignUp extends Component {
 
@@ -28,23 +27,11 @@ class SignUp extends Component {
 
   };
 
-  // handleLogin = (user) => {
-  //   const token = localStorage.getItem('token')
-  //   if (!!token) {
-  //     console.log("token present")
-  //   } else {
-  //     localStorage.setItem('token', user.jwt)
-  //   }
-  //   this.props.logIn(user)
-  // }
-
   render() {
     const { fields } = this.state;
-    // styles={{ backgroundImage: `url(${image})`}}
     return (
       <div id="home">
       <div className="ui segment center landing masthead">
-      {/* <div className="ui middle aligned center aligned grid"> */}
         <div className="ui text container">
           <h1 className="ui inverted header">Sign Up</h1>
         </div>
@@ -81,37 +68,8 @@ class SignUp extends Component {
         </form>
       </div>
       </div>
-
-
-      //  {/* <div id="home">
-      //   <h1>Sign Up!</h1>
-      //   <form onSubmit={this.handleSubmit}>
-      //     <div className="ui field">
-      //       <label>Email: </label>
-      //       <input
-      //         name="email"
-      //         value={fields.email}
-      //         onChange={this.handleChange}
-      //       />
-      //     </div>
-      //     <div className="ui field">
-      //       <label>Password: </label>
-      //       <input
-      //         name="password"
-      //         type="password"
-      //         value={fields.password}
-      //         onChange={this.handleChange}
-      //       />
-      //     </div>
-      //     <button type="submit" className="ui basic green button">
-      //       Sign Up
-      //     </button>
-      //   </form>
-      // </div> */}
-    );
   }
 }
 
 
-// export default SignUp;
 export default connect(null, { logIn, createNewUser })(SignUp);
