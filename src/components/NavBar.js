a6621b249e9df3e4fe920c2a2392d02f067f6dce
teasacura <36260380+teasacura@users.noreply.github.com>
2018-10-24 17:34:50 -0400

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,15 +4,6 @@ import { connect } from "react-redux";
 import { withRouter, NavLink } from 'react-router-dom'
 import { logOut, postSearch } from "../actions";
 
-// const link = {
-//   width: '100px',
-//   padding: '12px',
-//   margin: '0 6px 6px',
-//   background: 'blue',
-//   textDecoration: 'none',
-//   color: 'white',
-// }
-
 class NavBar extends Component {
 
   state = {
@@ -38,7 +29,6 @@ class NavBar extends Component {
 
 
   render(){
-    // console.log(this.props);
     return (
       <div>
         {this.props.loggedIn ? (
@@ -60,28 +50,9 @@ class NavBar extends Component {
               className="link item"
             >Foodr Search</NavLink>
             <div className="right menu">
-              {/* <div className="ui selection dropdown">
-                <input type="hidden" name="gender">
-                <i className="dropdown icon"></i>
-                <div className="default text">Gender</div>
-                <div className="menu">
-                  <div className="item" data-value="1">Male</div>
-                  <div className="item" data-value="1">Male</div>
-                  <div className="item" data-value="2">Female</div>
-                </div>
-              </div>
-              <select name="price" multiple="" className="ui selection dropdown">
-                <option value="1, 2, 3, 4">All Prices</option>
-                <option value="1">$</option>
-                <option value="2">$$</option>
-                <option value="3">$$$</option>
-                <option value="4">$$$$</option>
-              </select> */}
               <div className=" fluid item">
                 <div className="ui transparent icon input">
                   <input id="search" type="search" placeholder="Search Food Nearby..." onChange={this.handleChange} name="term"></input>
-                  {/* onClick={this.handleSubmit} */}
-                  {/* <Button circular icon='search' onClick={this.handleSubmit}/> */}
                   <i className="search link icon" onClick={this.handleSubmit}></i>
                 </div>
               </div>
@@ -104,9 +75,6 @@ class NavBar extends Component {
                 exact
                 className="link item"
               >Log In</NavLink>
-              {/* <a className="item">
-                Log In
-              </a> */}
             </div>
           </div>
         )}
@@ -114,29 +82,6 @@ class NavBar extends Component {
     )
   }
 }
-
-//   render(){
-//     return (
-//       <div id="nav">
-//         {this.props.loggedIn ? (
-//           <div>
-//             <div className="nav-link" onClick={this.handleLogout}>
-//               Log Out
-//             </div>
-//             <div className="nav-link">
-//               Profile
-//             </div>
-//           </div>
-//         ) : (
-//           null
-//         )}
-//         <div className="nav-link">
-//           Foodr Search
-//         </div>
-//       </div>
-//     )
-//   }
-// }
 
 
 function mapStateToProps(state) {
