8d9147d461c4da8f0c4a801969b9c355259009a0
teasacura <36260380+teasacura@users.noreply.github.com>
2018-10-24 17:37:03 -0400

    Update RestaurantListEl.js

--- src/components/RestaurantListEl.js
+++ src/components/RestaurantListEl.js
@@ -7,11 +7,9 @@ import { Rating, Button } from 'semantic-ui-react'
 const RestaurantListEl = (props) => {
     const { restaurant } = props
     return (
-      // className="ui segment"
       <div className="ui clearing segment" onClick={() => {props.selectRestaurant(restaurant)}}>
           <h4>{restaurant.name} <Rating defaultRating={restaurant.rating} maxRating={5}/>{restaurant.price}
             <Button size="tiny" floated='right'>{restaurant.distance}</Button>
-            {/* <div className="ui right floated button">{restaurant.distance}</div> */}
           </h4>
       </div>
     );
